918d992d8b9fb5cffd7fbe9e20b7e1b6ed944127
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-07-14 20:23:40 +0200

    React Props

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,24 @@ export function App() {
     return (
         <>
             <h1>Hello from React World !</h1>
-            <Greetings />
+            <Greetings 
+                firstName={"Rachel"} 
+                age={30} 
+                car={{color: "Red", 
+                speed: 110}} 
+                doSomething={
+                    function() {
+                        console.log("Hello !");
+                    }
+                }
+            >
+                {
+                    <img 
+                        src="https://cdn.pixabay.com/photo/2024/05/02/16/22/parrots-8735074_1280.jpg" 
+                        width={300}
+                    />
+                }
+            </Greetings>
         </>
     )
 }
--- src/Greetings.jsx
+++ src/Greetings.jsx
@@ -1,9 +1,11 @@
-export function Greetings() {
+export function Greetings(props) {
+    console.log("My Props", props);
+
     return (
         <ul>
-            <li>Hello</li>
-            <li>my</li>
-            <li>Friend</li>
+            <li>Hello {props.firstName},</li>
+            <li>You are {props.age} years old.</li>
+            <li>{props.children}</li>
         </ul>
     )
 }
