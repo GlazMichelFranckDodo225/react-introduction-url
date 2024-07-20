1ae1f6d2775135b197f07a7d1eaf6559cd22b728
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-07-20 06:21:25 +0200

    Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export function App() {
             <h1>Hello from React World !</h1>
             <Greetings 
                 firstName={"Rachel"} 
-                age={30} 
+                age={50} 
                 car={{color: "Red", 
                 speed: 110}} 
                 doSomething={
@@ -14,13 +14,9 @@ export function App() {
                         console.log("Hello !");
                     }
                 }
+                isSunny={false}
             >
-                {
-                    <img 
-                        src="https://cdn.pixabay.com/photo/2024/05/02/16/22/parrots-8735074_1280.jpg" 
-                        width={300}
-                    />
-                }
+                <img src="https://cdn.pixabay.com/photo/2024/05/02/16/22/parrots-8735074_1280.jpg" width={300} alt="Screen Shot" />
             </Greetings>
         </>
     )
--- src/Greetings.jsx
+++ src/Greetings.jsx
@@ -1,11 +1,20 @@
 export function Greetings(props) {
-    console.log("My Props", props);
-
     return (
         <ul>
             <li>Hello {props.firstName},</li>
             <li>You are {props.age} years old.</li>
             <li>{props.children}</li>
+            {/* <li>{props.isSunny && "It's nice weather"}</li> */}
+            {/* <li>{props.isSunny && <ItsSunny />}</li> */}
+            <li>{props.isSunny ? <ItsSunny /> : <ItsRainy />}</li>
         </ul>
     )
+}
+
+function ItsSunny() {
+    return <h1>It's nice weather</h1>
+}
+
+function ItsRainy() {
+    return <h1>It's raining</h1>
 }
