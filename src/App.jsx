ae982fefcb4f56286314f16cfa1b0310b6bdc92c
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-03 05:45:15 +0200

    Stying with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,12 @@
-// import { Greetings } from "./Greetings";
-// import { Bank } from "./exos/Bank";
-
-import { useState } from "react";
-// import { AgeCounter } from "./AgeCounter";
-
 export function App() {
-    const [movie, setMovie] = useState("The Lord of the Rings");
+    const a = false;
 
-    function updateMovie() {
-        setMovie("Fight Club");
-
-        console.log(movie);   
-    }
-    
-    console.log(movie);
-       
-    console.log("App Component Rerendering");
-    
     return (
-        <>
-            {/* <AgeCounter /> */}
-            <button onClick={updateMovie}>{movie}</button>
-        </>
+        <div style={{
+            backgroundColor: "red",
+            height: a ? 100 : 200,
+            width: 100,
+            border: "3px solid blue"
+        }}></div>
     )
-
-    /* return <div>
-        <Bank nomBanque="Banque centrale" />
-    </div> */
-
-    /* (
-        <>
-            <h1>Hello from React World !</h1>
-            <Greetings 
-                firstName={"Rachel"} 
-                age={50} 
-                car={{color: "Red", 
-                speed: 110}} 
-                doSomething={
-                    function() {
-                        console.log("Hello !");
-                    }
-                }
-                isSunny={false}
-            >
-                <img src="https://cdn.pixabay.com/photo/2024/05/02/16/22/parrots-8735074_1280.jpg" width={300} alt="Screen Shot" />
-            </Greetings>
-        </>
-    ) */
 }
